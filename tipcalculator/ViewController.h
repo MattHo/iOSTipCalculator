714d071470f9f25bd3b04ba0066bf0470e97660d
Matt Ho <mattho@yahoo-inc.com>
2015-01-19 15:34:00 -0800

    Initial Commit

---
+++ tipcalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tipcalculator
+//
+//  Created by Matt Ho on 1/19/15.
+//  Copyright (c) 2015 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tipcalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Matt Ho on 1/19/15.
+//  Copyright (c) 2015 Yahoo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
